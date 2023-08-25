32e67a4d0c98cf23cfdba52cad4943fa6c73413a
BrenenHatch <105125784+BrenenHatch@users.noreply.github.com>
2023-08-25 16:42:01 -0400

    Add files via upload

---
+++ notes-app/app.js
@@ -0,0 +1,68 @@
+const validator = require('validator')
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
+
+const log = console.log;
+
+yargs.version('1.1.0')
+
+//create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Body for note',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.addNote(argv.title, argv.body);
+    }
+})
+
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.removeNote(argv.title);
+    }
+})
+
+//create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler() {
+        console.log('Reading the note')
+    }
+})
+
+//create list command
+yargs.command({
+    command: 'list',
+    describe: 'Listing all the notes',
+    handler(){
+        console.log('Listing the notes')
+    }
+})
+
+
+yargs.parse();
+// log(yargs.argv)
+
