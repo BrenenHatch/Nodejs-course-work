b69d9d4821f0d75c4a577af43aa37e3576173ec3
Brenen Hatch <brenen.hatch@gtnllc.com>
2023-08-28 10:40:50 -0400

    Finished notes-app

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,18 +1,25 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-const getNotes = () => {
-    return 'Your notes...'
-}
+const readNote = (title) => {
+    const notes =loadNotes();
+    const note = notes.find((note) => note.title === title)
 
+    if (note){
+        console.log(chalk.italic(note.title))
+        console.log(note.body)
+    } else {
+        console.log(chalk.red.inverse('Note not found!'))
+    }
+}
 
 
 const addNote = (title, body) => {
     const notes = loadNotes();
 
-    const duplicateNotes = notes.filter(note => note.title === title);
+    const duplicateNote = notes.find((note) => note.title === title)
 
-    if (duplicateNotes.length === 0){
+    if (!duplicateNote){
         notes.push({
             title: title,
             body: body
@@ -68,8 +75,8 @@ const loadNotes = () => {
 }
 
 module.exports = {
-    getNotes: getNotes,
     addNote: addNote,
     removeNote: removeNote,
-    listNotes: listNotes
+    listNotes: listNotes,
+    readNote: readNote
 }
